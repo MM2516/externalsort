5c5da99fdbad352f2538f566c66ecf445da6283c
MM2516 <90863728+MM2516@users.noreply.github.com>
2022-04-25 12:57:49 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace stdl
+
+int main(){
+    int b, rp, r;
+    cin>>b;  //no of buffer pages
+    cin>>rp;  //no of records per page
+    cin>>r;  //no of total records in a datafile  
+    
